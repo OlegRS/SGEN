5107be048fd49d1986fb22fd7efcc378b96f2305
OlegRS <oleg.r.senkevich@gmail.com>
2025-03-13 11:23:07 +0000

    chore: Update setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+# Install required dependencies
+apt install -y cmake
+pip install pybind11 numpy pyvista
+
+# Find where Pybind11 is installed
+python3 -m pybind11 --cmakedir
+
 pip install .
