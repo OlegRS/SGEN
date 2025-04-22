590227cd28ed3ec00fb0533883c1920645c45733
OlegRS <oleg.r.senkevich@gmail.com>
2025-04-22 02:13:51 +0100

    chore: Change units to microns and hours everywhere to ensure consistency

--- src/junctions/Som_den_junction.cpp
+++ src/junctions/Som_den_junction.cpp
@@ -9,14 +9,14 @@ Junction& Neuron::Som_den_junction::set_hopping_rate_constants() {
     trafficking_scaling_factor = ( 1/(p_from->length) + 1/(p_to->length) )/2.;
 
   double
-    mRNA_fwd_diff_rate = p_from->mRNA_diffusion_constant * 3600 * diffusion_scaling_factor,
-    mRNA_bkwd_diff_rate = p_to->mRNA_diffusion_constant * 3600 * diffusion_scaling_factor,
-    mRNA_fwd_traff_rate = p_from->mRNA_forward_trafficking_velocity * 3600 * trafficking_scaling_factor,
-    mRNA_bkwd_traff_rate = p_to->mRNA_backward_trafficking_velocity * 3600 * trafficking_scaling_factor,
-    prot_fwd_diff_rate = p_from->protein_diffusion_constant * 3600 * diffusion_scaling_factor,
-    prot_bkwd_diff_rate = p_to->protein_diffusion_constant * 3600 * diffusion_scaling_factor,
-    prot_fwd_traff_rate = p_from->protein_forward_trafficking_velocity * 3600 * trafficking_scaling_factor,
-    prot_bkwd_traff_rate = p_to->protein_backward_trafficking_velocity * 3600 * trafficking_scaling_factor;
+    mRNA_fwd_diff_rate = p_from->mRNA_diffusion_constant * diffusion_scaling_factor,
+    mRNA_bkwd_diff_rate = p_to->mRNA_diffusion_constant * diffusion_scaling_factor,
+    mRNA_fwd_traff_rate = p_from->mRNA_forward_trafficking_velocity * trafficking_scaling_factor,
+    mRNA_bkwd_traff_rate = p_to->mRNA_backward_trafficking_velocity * trafficking_scaling_factor,
+    prot_fwd_diff_rate = p_from->protein_diffusion_constant * diffusion_scaling_factor,
+    prot_bkwd_diff_rate = p_to->protein_diffusion_constant * diffusion_scaling_factor,
+    prot_fwd_traff_rate = p_from->protein_forward_trafficking_velocity * trafficking_scaling_factor,
+    prot_bkwd_traff_rate = p_to->protein_backward_trafficking_velocity * trafficking_scaling_factor;
     
   fwd_mRNA_hop_rate = (mRNA_fwd_diff_rate + mRNA_fwd_traff_rate);///static_cast<Soma*>(p_from)->n_descending_DS;
   bkwd_mRNA_hop_rate = mRNA_bkwd_diff_rate + mRNA_bkwd_traff_rate;
